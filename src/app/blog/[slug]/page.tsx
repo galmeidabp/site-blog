c9cda5f6fee77db548bf3cef8406c9ff1a6a5ae5
Gabriella Almeida <galmeidabp@gmail.com>
2025-06-05 17:18:30 -0300

    page remove params

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -2,27 +2,29 @@ import { allPosts } from ".contentlayer/generated";
 import { PostPage } from "@/templates/blog";
 import { notFound } from "next/navigation";
 
-type BlogPostPageProps = {
+type BlogPostPage = {
   params: {
     slug: string;
   };
 };
 
+
 export const revalidate = 60;
 
 export async function generateStaticParams() {
-  return allPosts
-    .filter((post) => !!post.slug)
-    .map((post) => ({
-      slug: post.slug,
-    }));
+  return allPosts.map((post) => ({
+    slug: post.slug,
+  }));
 }
 
-export default async function BlogPostPage({ params }: BlogPostPageProps) {
+export default async function BlogPostPage({ params }: BlogPostPage) {
   const { slug } = params;
   const post = allPosts.find((post) => post.slug === slug);
 
-  if (!post) return notFound();
+  if (!post) {
+    notFound();
+  }
 
   return <PostPage post={post} />;
 }
+
